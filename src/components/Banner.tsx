3c3cdfb06854db4c19eb3be613861e8c402cfab0
Gustavo Felipe <gustavo.ffelipedev@gmail.com>
2022-09-17 08:36:19 -0300

    banner update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -14,8 +14,8 @@ export function Banner(){
             
             <img className={style.perfil} src={PerfilPhoto} alt="" />
             <div className={style.bannerText}>
-                <h2 className={style.blueText}>FRONT-END DESIGN.</h2>
-                <h2 className={style.blackText}>USER INTERFACE.</h2>
+                <h2 className={style.blueText}>FRONT-END DEVELOPER.</h2>
+                <h2 className={style.blackText}>UX/UI DESIGNER.</h2>
                 <a href="#contact"><button className={style.buttonPopForm} 
                 > 
                 Vamos nessa
